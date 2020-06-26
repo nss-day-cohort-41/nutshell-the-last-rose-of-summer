769b9b0e8f70a77bf02117d207d66a0fa947a242
davidbrucetn <severian37@gmail.com>
2020-06-26 15:40:35 -0500

    Added events.js, portal.js, CSS cards for items

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+import includeHTML from './portal.js'
+import { updateAllScrolls } from './events.js'
+
+
+
+//pass html page to insert into main element 
+let activeUser = true
+if (!activeUser) {
+    const main = document.querySelector(".container__main")
+    main.innerHTML=""
+    includeHTML("login.html");
+
+}
+
+//call to update scroll when message is added or edited
+const messageContainer = document.querySelector(".container__messages--saved")
+updateAllScrolls();
