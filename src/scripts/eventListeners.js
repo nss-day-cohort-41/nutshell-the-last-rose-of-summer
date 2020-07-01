0960a082a758d671599bc989b14d2aced89f1cb7
John Hester <hesterusmaximus@gmail.com>
2020-07-01 14:42:09 -0500

    added edit button for tasks

--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -217,7 +217,39 @@ const listeners = {
             }
         })
     },
+    editTaskListener () {
+        document.querySelector(".container__main__right--tasks").addEventListener("click", event => {
 
+            if(event.target.id.startsWith("buttonTask__edit--")) {
+
+                const taskId = event.target.id.split("--")[1]
+
+                taskItem.editTask(taskId)
+            }
+
+        })
+    },
+    updateTaskInDatabase () {
+        document.querySelector("#button__edit--task").addEventListener("click", event => {
+
+            const editTaskObj = {}
+            //grabs data from new task field
+            editTaskObj.userId = document.getElementById("field__task__userId").value
+            editTaskObj.task = document.getElementById("task__title").value
+            editTaskObj.completionDate = document.getElementById("task__date").value
+            editTaskObj.complete = false
+            editTaskObj.id = document.getElementById("field__task__taskId").value
+
+            editTaskObj.userId = parseInt(editTaskObj.userId, 10)
+            editTaskObj.id = parseInt(editTaskObj.id, 10)
+            
+
+            API.editUserTask(editTaskObj.id, editTaskObj)
+                .then(taskItem.taskListGenerator)
+                .then(shared.clearDataField)
+
+        })
+    },
 
     //Friends section event listeners
     enableFriendDelete() {
